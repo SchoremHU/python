16d17f157241255b911cb104621fdbb3b1c7d0fd
hudaxian <35534945+SchoremHU@users.noreply.github.com>
2020-03-31 07:56:01 +0800

    Add files via upload

---
+++ python.py
@@ -0,0 +1,36 @@
+# for i in range (1,10):
+#     for j in range (1,i+1):
+#         print(str(j)+'*'+str(i)+'='+str(i*j)+'\t',end='')
+#     print()
+# for i in range (1,10):
+#     line =''
+#     for j in range (1,i+1):
+#         line += '{}*{}={:<4}'.format(j,i,i*j)
+#     print(line)
+# for i in range (1,10):
+#     for j in range (1,i+1):
+#         pro = i*j 
+#         if j > 1 and pro < 10:
+#             pro = str(pro)+' '
+#         else:
+#             pro = str(pro)
+
+#         print(str(j)+'*'+str(i)+'='+pro,end=' ')
+#     print()
+
+# for i in range(1,10):
+#     line = ''
+#     for j in range(1,10):
+#         if i > j:
+#             line = '{} {} {:<4}'.format(' ',' ',' ')
+#         else:
+#             line = '{}*{}={:<4}'.format(i,j,i*j)
+#         print (line,end=' ')
+#     print()
+
+for i in range(-3,4):
+    if i < 0:
+        pro = -i
+    else:
+        pro = i
+    print(' '*pro + '*'*(7-pro*2))
