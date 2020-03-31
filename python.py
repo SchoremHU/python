4b14f88ba3711f3385fa982c48be8253e3b08182
hudaxian <whym21@outlook.com>
2020-03-31 23:48:02 +0800

    Update python.py

--- python.py
+++ python.py
@@ -44,15 +44,43 @@
 #         print('*'*7)
 
 
+# 100以内斐波那契数列
             # x = (x-1)+(x-2) , x>=1
-x = 0
-y = 1
-print (0)
-print (1)
-while True:
-    k = x+y
-    if k > 100:break
-    x = y
-    y = k
-    print(k)
+# x = 0
+# y = 1
+# print (0)
+# print (1)
+# while True:
+#     k = x+y
+#     if k > 100:break
+#     x = y
+#     y = k
+#     print(k)
 
+# 第101项斐波那契数列
+# x = 0
+# y = 1
+# z = 1
+# print (0)
+# print (1)
+# while True:
+#     k = x + y
+#     z += 1
+#     if z == 102:break
+#     x = y
+#     y = k
+#     print(k,z)
+
+
+# 10万内质数
+    # 10以内质数，步骤记录分析
+y = 2 #为了打印出出事 y 值
+for x in range(2,10,1):
+    print('__________')
+    print('x:',x,'y:',y)
+    for y in range(2,x,1):  #此时跳过次循环，直接打印 x2- ，range（2,2,1）为合法的空的迭代器
+        print ('**** x1',x , y)
+        print('****', x % y == 0)
+        if x % y == 0:
+            break #用 x1 依次对 y 内的所有数取模，直到满足条件跳过并进入else打印x2-，否则进入 for y 循环
+    else: print('x2-',x)
